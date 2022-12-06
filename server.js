33d1354a0439415d811fc41aea908a134e6f65e2
MOHAMED ALI FAWZI <76850143+MohamedAli00949@users.noreply.github.com>
2022-12-06 22:04:53 +0200

    Get one memory

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
-app.use("/api/", async (req, res) => {
+app.get("/api/", async (req, res) => {
   res.send("<h1>Hello, world</h1>");
 });
 
@@ -31,6 +31,30 @@ app.get("/api/memories/", async (req, res) => {
       currentPage: page,
       numberOfPages: Math.ceil(memoriesData.length / realLimit)
     });
+
+  } catch (error) {
+    res
+      .status(500)
+      .json({
+        message:
+          error?.message || "Some thing wrong at server or your code"
+      });
+    console.log(error);
+  }
+});
+
+app.get('/api/memories/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const data = Data.findOne(Number(id));
+
+    if (data === -1)
+      return res
+        .status(404)
+        .json({ message: "Not founded data for this id." });
+
+    res.status(200).json(data);
 
   } catch (error) {
     res
