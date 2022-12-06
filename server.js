805ed9ea58f770a2ecd74b1ef141a26b90925d06
MOHAMED ALI FAWZI <76850143+MohamedAli00949@users.noreply.github.com>
2022-12-06 21:47:26 +0200

    Get all memories

--- server.js
+++ server.js
@@ -14,4 +14,33 @@ app.use("/api/", async (req, res) => {
   res.send("<h1>Hello, world</h1>");
 });
 
+const Data = require("./db/fackMemorise.js");
+// Endpoint
+app.get("/api/memories/", async (req, res) => {
+  try {
+    const { page, limit } = req.query;
+    const memoriesData = Data.findAll(),
+      realPage = Number(page) - 1 || 0,
+      realLimit = Number(limit) || 25,
+      startIndex = realLimit * realPage,
+      data = memoriesData.slice(startIndex, startIndex + realLimit);
+
+    res.status(200).json({
+      data,
+      total: memoriesData.length,
+      currentPage: page,
+      numberOfPages: Math.ceil(memoriesData.length / realLimit)
+    });
+
+  } catch (error) {
+    res
+      .status(500)
+      .json({
+        message:
+          error?.message || "Some thing wrong at server or your code"
+      });
+    console.log(error);
+  }
+});
+
 module.exports = app;
